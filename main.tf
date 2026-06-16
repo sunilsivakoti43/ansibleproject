530117f303e3b34f523e9ab1cc090402f18b3092
sunilsivakoti43 <sunilsivakoti43@gmail.com>
2026-06-16 16:10:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-01a00762f46d584a1"
 instance_type = "t3.micro"
-key_name = "anisble"
-vpc_security_group_ids = ["sg-0dbac3cf2f9a38860"]
+key_name = "anisbler"
+vpc_security_group_ids = ["sg-0695ca6efe7293e4f"]
 tags = {
 Name = var.instance_names[count.index]
 }
