dc4b71f9d95ce173ea488795d89def947cf03cbb
sunilsivakoti43 <sunilsivakoti43@gmail.com>
2026-06-16 11:29:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 resource "aws_instance" "one" {
 count = 4
 ami = "ami-0e38835daf6b8a2b9"
-instance_type = "t2.medium"
+instance_type = "t3.micro"
 key_name = "anisble"
 vpc_security_group_ids = ["sg-0292a613d304fd897"]
 tags = {
