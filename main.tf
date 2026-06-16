897c6b285a870c8fcba26c7d714bdc0083ad4e9a
sunilsivakoti43 <sunilsivakoti43@gmail.com>
2026-06-16 15:57:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-01a00762f46d584a1"
 instance_type = "t3.micro"
 key_name = "anisble"
-vpc_security_group_ids = ["sg-0292a613d304fd897"]
+vpc_security_group_ids = ["sg-0dbac3cf2f9a38860"]
 tags = {
 Name = var.instance_names[count.index]
 }
